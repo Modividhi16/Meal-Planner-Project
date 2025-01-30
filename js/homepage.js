71439589ece49579c1a0c7ec6102ace0ff9f1d8e
Vidhi Modi <modividhi1695@gmail.com>
2025-01-30 17:17:20 -0600

    minor updates on css and homepage

--- js/homepage.js
+++ js/homepage.js
@@ -3,21 +3,20 @@
     const signInForm = document.querySelector('#signInForm');
 
     document.querySelector('#linkCreateAccount').addEventListener('click' , (event) => {
-      event.preventDefault;
+      event.preventDefault();
       signInForm.classList.add("form-hidden");
       signUpForm.classList.remove("form-hidden");
     })
 
     document.querySelector('#linkLogin').addEventListener('click' , (event) => {
-      event.preventDefault;
+      event.preventDefault();
       signInForm.classList.remove("form-hidden");
       signUpForm.classList.add("form-hidden");
     })
 
    })  
-    
-    //const signUpForm = document.getElementById('signUpForm');
-    
+   
+    //SignUp form    
      signUpForm.addEventListener('submit' , function(event){
      event.preventDefault();
      const name = document.getElementById('name').value;
@@ -39,6 +38,35 @@
         window.location.href = 'meal_planner.html';
      }
      })
+
+     //signIn form 
+     signInForm.addEventListener('submit' , function(event){
+      event.preventDefault();
+      console.log("inside signinform")
+      const signInEmail = document.getElementById('signInEmail').value;
+      const signInPassword = document.getElementById('signInPassword').value;
+  
+
+  
+      if( signInEmail && signInPassword){         
+         if(signInPassword.length < 8){
+             document.getElementById('error-password').textContent = "Password must be 8 character long";
+             return;
+         }
+         const hasNumber = /\d/; // Regular expression to match at least one digit
+         if (!hasNumber.test(signInPassword)) {
+         document.getElementById('error-password').textContent = "Password must contain at least one number!";
+         return;
+       }
+         window.location.href = 'meal_planner.html';
+      }
+      })
+
+    
+
+
+
+
 
 
    
